2c2fddd439a3de45e83a92456395908b411bea08
Peter Kleinjan <peterkleinjan@gmail.com>
2015-01-25 15:33:44 +0100

    Bestelling + klant klasses gevuld

--- src/Bestelling.java
+++ src/Bestelling.java
@@ -1,5 +1,35 @@
+import java.util.Date;
+
 /**
  * Created by Peter on 25-1-2015.
  */
 public class Bestelling {
+    private int klantID;
+    private int bestellingID;
+    private boolean verwerking;
+    private Date starttijd;
+    private int duur;
+    private boolean compleet;
+    private boolean dadelijk;
+
+    public void Bestelling(int klantID, int bestellingID, boolean dadelijk ){
+        this.klantID = klantID;
+        this.bestellingID = bestellingID;
+        this.dadelijk = dadelijk;
+        this.verwerking = false;
+        this.compleet = false;
+    }
+
+    public void startVerwerking(){
+        this.verwerking = true;
+        this.starttijd = new Date();
+    }
+
+    public void eindVerwerking(){
+
+        //Weet niet zeker of deze goed werkt:
+        this.duur = new Date().compareTo(starttijd);
+
+        this.compleet = true;
+    }
 }
--- src/Klant.java
+++ src/Klant.java
@@ -2,4 +2,83 @@
  * Created by Peter on 25-1-2015.
  */
 public class Klant {
+    private int klantID;
+    private String achternaam;
+    private String tussenvoegsel;
+    private String voornaam;
+    private int leeftijd;
+    private char geslacht;
+    private String plaats;
+    private String email;
+
+    public void Klant(int klantID, String achternaam, String tussenvoegsel, String voornaam, int leeftijd, char geslacht, String plaats, String email){
+        this.klantID = klantID;
+        this.achternaam = achternaam;
+        this.tussenvoegsel = tussenvoegsel;
+        this.voornaam = voornaam;
+        this.leeftijd = leeftijd;
+        this.geslacht = geslacht;
+        this.plaats = plaats;
+        this.email = email;
+    }
+
+    public int getKlantID() {
+        return klantID;
+    }
+
+    public String getAchternaam() {
+        return achternaam;
+    }
+
+    public void setAchternaam(String achternaam) {
+        this.achternaam = achternaam;
+    }
+
+    public String getTussenvoegsel() {
+        return tussenvoegsel;
+    }
+
+    public void setTussenvoegsel(String tussenvoegsel) {
+        this.tussenvoegsel = tussenvoegsel;
+    }
+
+    public String getVoornaam() {
+        return voornaam;
+    }
+
+    public void setVoornaam(String voornaam) {
+        this.voornaam = voornaam;
+    }
+
+    public int getLeeftijd() {
+        return leeftijd;
+    }
+
+    public void setLeeftijd(int leeftijd) {
+        this.leeftijd = leeftijd;
+    }
+
+    public char getGeslacht() {
+        return geslacht;
+    }
+
+    public void setGeslacht(char geslacht) {
+        this.geslacht = geslacht;
+    }
+
+    public String getPlaats() {
+        return plaats;
+    }
+
+    public void setPlaats(String plaats) {
+        this.plaats = plaats;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
